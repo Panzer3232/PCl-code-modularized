5be27a55613e11c2f8f33006f6a33c43c9887d2e
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2025-06-20 00:42:32 +0200

    Delete train1.py

--- train1.py
+++
@@ -1,5 +0,0 @@
-# train.py
-if __name__ == "__main__":
-    print("✅ [Mock] train.py ran successfully.")
-    print("ℹ️  This is a fake placeholder for testing Docker and Makefile.")
-    print("🔄 To use real training, rename train1.py to train.py or update Dockerfile/Makefile.")
